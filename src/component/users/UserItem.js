24a07c1a84f673ffb2034132cd890889a25a2103
tunguyen-95 <thunguyen.tt@outlook.com>
2019-11-18 13:25:18 +0100

    display user profile

---
+++ src/component/users/UserItem.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class UserItem extends Component {
+  state = {
+    id: 'id',
+    login: 'simonjefford',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/136?v=4',
+    html_url: 'https://github.com/simonjefford'
+  };
+
+  render() {
+    const { login, avatar_url, html_url } = this.state;
+    return (
+      <div className="card text-center">
+        <img
+          src={avatar_url}
+          className="round-img"
+          style={{ width: '60px' }}
+          alt=""
+        ></img>
+        <h3>{login}</h3>
+        <div>
+          <a href={html_url} className="btn btn-dark btn-sm my-1">
+            More
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UserItem;
