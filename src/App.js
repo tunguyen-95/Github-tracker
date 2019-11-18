181f5d3456cf59765cc8ea1a5dade7369720a51a
tunguyen-95 <thunguyen.tt@outlook.com>
2019-11-18 22:25:40 +0100

    change the display of clear button

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ class App extends Component {
   };
 
   render() {
+    const { users, loading } = this.state;
     return (
       <div className="App">
         <Navbar />
@@ -38,9 +39,9 @@ class App extends Component {
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
-            showClear={this.state.users.length > 0 ? true : false}
+            showClear={users.length > 0 ? true : false}
           />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
