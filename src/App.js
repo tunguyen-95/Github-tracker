415c7237945af0f52c0ee40e8253eb1366348388
tunguyen-95 <thunguyen.tt@outlook.com>
2019-11-20 10:45:27 +0100

    add User component

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
       `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
-    this.setState({ users: res.data, loading: false });
+    this.setState({ user: res.data, loading: false });
   };
   //Clear profile from the state
   clearUsers = () => {
